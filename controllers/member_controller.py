541406692ee51496fc2067f097e2732a56801be2
hollyhbrady <hollyhbrady@gmail.com>
2021-04-23 18:29:15 +0100

    member controller imports and functions named

---
+++ controllers/member_controller.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request, redirect
+from flask import Blueprint
+from models.member import Member
+import repositories.member_repository as member_repository
+
+members_blueprint = Blueprint("members", __name__)
+
+@members_blueprint.route("/members")
+def members():
+
+
+@members_blueprint.route("/members/<id>")
+def show_member(id):
