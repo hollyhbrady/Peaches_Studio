eb53cb75d3de6710e1b3d1055c7cbc1fef75a217
hollyhbrady <hollyhbrady@gmail.com>
2021-04-23 17:55:39 +0100

    booking repo imports and functions named

---
+++ repositories/booking_repository.py
@@ -0,0 +1,23 @@
+from db.run_sql import run_sql
+
+from models.booking import Booking
+from models.lesson import Lesson
+from models.member import Member
+
+import repositories.member_repository as member_repository
+import repositories.lesson_repository as lesson_repository
+
+def save(booking):
+    pass
+
+
+def select_all():
+    pass
+
+
+def delete_all():
+    pass
+
+
+def delete(id):
+    pass
