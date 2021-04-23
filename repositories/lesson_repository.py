cd7fbfc60ca3273e8f5b399bb1f4d016d475bfd3
hollyhbrady <hollyhbrady@gmail.com>
2021-04-23 18:02:32 +0100

    lesson repo imports and functions named

---
+++ repositories/lesson_repository.py
@@ -0,0 +1,27 @@
+from db.run_sql import run_sql
+
+from models.booking import Booking
+from models.lesson import Lesson
+from models.member import Member
+
+import repositories.member_repository as member_repository
+import repositories.booking_repository as booking_repository
+
+def save(lesson):
+    pass
+
+
+def select_all():
+    pass
+
+
+def select(id):
+    pass
+
+
+def delete(id):
+    pass
+
+
+def delete_all():
+    pass
