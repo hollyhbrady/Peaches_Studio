b807f3c8b7207835ece176beab35233b20b45a25
hollyhbrady <hollyhbrady@gmail.com>
2021-04-23 17:21:24 +0100

    Lesson Class

---
+++ models/lesson.py
@@ -0,0 +1,10 @@
+class Lesson:
+
+    def __init__(self, name, capacity, category, day, time, duration, id = None):
+        self.name = name
+        self.capacity = capacity
+        self.category = category 
+        self.day = day
+        self.time = time
+        self.duration = duration
+        self.id = id
