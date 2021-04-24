6647a815cb5fe27bfe69533f649b19c0ca4884dc
hollyhbrady <hollyhbrady@gmail.com>
2021-04-24 20:58:43 +0100

    booking repo - save(booking)

--- repositories/booking_repository.py
+++ repositories/booking_repository.py
@@ -8,7 +8,11 @@ import repositories.member_repository as member_repository
 import repositories.lesson_repository as lesson_repository
 
 def save(booking):
-    pass
+    sql = "INSERT INTO bookings (member_id, lesson_id) VALUES (%s, %s) RETURNING id"
+    values = [booking.member.id, booking.lesson.id]
+    results = run_sql(sql, values)
+    booking.id = results[0]['id']
+    return booking
 
 
 def select(id):
