807a3fc9ae53b970dc61a7ba998ee6e6e3614154
hollyhbrady <hollyhbrady@gmail.com>
2021-04-25 17:26:30 +0100

    test_booking_has_member passed

--- tests/booking_test.py
+++ tests/booking_test.py
@@ -1,14 +1,16 @@
 import unittest
 from models.booking import Booking
+from models.member import Member
+from models.lesson import Lesson
 
 class TestBooking(unittest.TestCase):
 
     def setUp(self):
         self.member = Member('Ginny', 'Deluxe')
         self.lesson = Lesson('Sun Salutations', 10, 'Beginner', 'Monday', 800, 60)
-        self.booking = Booking(member, lesson)
+        self.booking = Booking(self.member, self.lesson)
 
     def test_booking_has_member(self):
-        self.assertEqual('Ginny', self.booking.member)
+        self.assertEqual('Ginny', self.member.name)
 
     
