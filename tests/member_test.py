1e51112690f58689b77985514b4fbdec7f75a75f
hollyhbrady <hollyhbrady@gmail.com>
2021-04-25 17:48:08 +0100

    test_member_has_membership

--- tests/member_test.py
+++ tests/member_test.py
@@ -8,3 +8,6 @@ class TestMember(unittest.TestCase):
 
     def test_member_has_name(self):
         self.assertEqual('Ginny', self.member.name)
+
+    def test_member_has_membership(self):
+        self.assertEqual('Deluxe', self.member.membership)
