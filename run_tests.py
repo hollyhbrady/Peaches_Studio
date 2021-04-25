ebe1838dcffd194497d1c8caaf14ea3379b5bd87
hollyhbrady <hollyhbrady@gmail.com>
2021-04-25 17:04:54 +0100

    run_test set up

--- run_tests.py
+++ run_tests.py
@@ -1,1 +1,7 @@
 import unittest
+from tests.lesson_test import TestLesson
+from tests.member_test import TestMember
+from tests.booking_test import TestBooking
+
+if __name__ == '__main__':
+    unittest.main()
