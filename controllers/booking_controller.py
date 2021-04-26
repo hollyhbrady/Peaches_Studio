0f6d9f7965114a9d0aeb8609d954e86f22a09740
hollyhbrady <hollyhbrady@gmail.com>
2021-04-26 19:30:55 +0100

    booking cont - new booking GET done

--- controllers/booking_controller.py
+++ controllers/booking_controller.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, redirect
 from flask import Blueprint
 from models.booking import Booking
 import repositories.booking_repository as booking_repository
+import repositories.member_repository as member_repository
+import repositories.lesson_repository as lesson_repository
 
 
 bookings_blueprint = Blueprint("bookings", __name__)
@@ -11,9 +13,11 @@ def bookings():
     bookings = booking_repository.select_all()
     return render_template("bookings/index.html", bookings = bookings)
 
-
-# @bookings_blueprint.route("/bookings/new", methods=['GET'])
-# def new_booking():
+@bookings_blueprint.route("/bookings/new", methods=['GET'])
+def booking_form():
+    members = member_repository.select_all()
+    lessons = lesson_repository.select_all()
+    return render_template("/bookings/new.html", title="Add Booking", members = members, lessons = lessons)
 
 
 # @bookings_blueprint.route("/bookings", methods=['POST'])
